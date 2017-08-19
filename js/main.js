46e9437c60d64940ce222b5f874b42f3b7babdad
kwanwooi25 <kwanwoo.jeong@gmail.com>
2017-08-20 00:28:51 +0900

    added the comments for js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-// Search
+/* ==================================================
+ Search Box
+================================================== */
+
 $('.input-search').on('keyup',function() {
   var $searchText = $(this).val().toLowerCase();
-  if ($searchText === '') {
-    $('img').parent().show();
-  } else {
-    $('img').parent().hide();
+  if ($searchText === '') {     // if nothing is typed on search field,
+    $('img').parent().show();   // show all the images
+  } else {                      // if input is not null,
+    $('img').parent().hide();   // hide all the images first,
     $('img[alt*=' + $searchText + ']').parent().show();
+                                /* then only show the images
+                                   when the images' alt attribute includes
+                                  the text input on search field. */
   }
 });
 
-//Lightbox - lightGallery
+
+/* ==================================================
+ Lightbox Gallery
+================================================== */
+
 var $lg = $('.gallery');
 $lg.lightGallery({
   mode: 'lg-fade',
